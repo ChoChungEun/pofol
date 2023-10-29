fd796897070aeee91dd079e263696e2694bd31bf
ChoChungEun <82curst@gmail.com>
2023-10-29 15:19:47 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+  return <div className="App">test</div>;
 }
 
 export default App;
